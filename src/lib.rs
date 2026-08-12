8cde91d5e78551d42b6c5739058887122260447a
Travis Brown <travisrobertbrown@gmail.com>
2026-08-12 16:06:49 +0200

    Add lints

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 #![deny(missing_docs)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, rust_2018_idioms)]
+#![allow(clippy::missing_errors_doc)]
+#![forbid(unsafe_code)]
 //! A WARC (Web ARChive) library
 
 const MB: usize = 1_048_576;
